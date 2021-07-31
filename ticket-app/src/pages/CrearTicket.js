e8d90cceb00e855287c7a359da1153f5e172438d
JulioAvalos <julioavalos2@msn.com>
2021-07-31 14:26:52 -0600

    adding style for ticket creation component

--- ticket-app/src/pages/CrearTicket.js
+++ ticket-app/src/pages/CrearTicket.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Typography, Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 export const CrearTicket = () => {
-    return (
-        <div>
-            CrearTicket
-        </div>
-    )
-}
+  const nuevoTicket = () => {
+    console.log("nuevoTicket");
+  };
+
+  return (
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>Presione el boton para un nuevo ticket</Title>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined />}
+            size="large"
+            onClick={nuevoTicket}
+          >
+            Nuevo Ticket
+          </Button>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Su numero</Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }}>
+            55
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
+};
